2b1bd1f371d5286497dbe93af48105bfdc0c2354
Ubuntu <ubuntu@ip-172-31-15-108.ec2.internal>
2020-08-24 17:21:36 +0000

    version of run_lds that runs

--- main/run_lds.sh
+++ main/run_lds.sh
@@ -1,46 +1,64 @@
 #!/bin/bash
 ## Get the path to this function:
-
 execpath="$0"
 echo $execpath
-scriptpath="$neurocaasrootdir/ncap_utils"
+scriptpath="/home/ubuntu/NeuroCAAS/LDS_algo/neurocaas_remote/ncap_utils"
 
-## Import functions for workflow management
+source "$scriptpath/paths.sh"
+
+## Now declare remote directory name for here and all sourced files: 
+set -a
+neurocaasrootdir="/home/ubuntu/NeuroCAAS/LDS_algo/neurocaas_remote"
+set +a
+
 source "$scriptpath/workflow.sh"
-## Import functions for data transfer
+## Import functions for data transfer 
 source "$scriptpath/transfer.sh"
 
-
-errorlog
-errorlog_init
-
-## Set up STDOUT and STDERR Monitoring:
-errorlog_background &
-background_pid=$!
-echo $background_pid, "is the pid of the background process"
-
-
-
-## Now parse arguments in to relevant variables. These will be available to all scripts run from within.
+## Now parse arguments in to relevant variables. These will be available to all scripts run from within. 
 # Bucket Name  $bucketname
-# Group Directory $groupdir
+# Group Directory $groupdir 
 # Results Directory $resultdir
 # Analysis Output Directory $process
 # Dataset Name (without path) $dataname
 # Dataset Full Path $datapath
 # Configuration Name # configname
 # Configuration Path # configpath
+set -a
+parseargsstd "$1" "$2" "$3" "$4"
+set +a
+
+echo $bucketname >> "/home/ubuntu/check_vars.txt" 
+echo $groudir >> "/home/ubuntu/check_vars.txt" 
+echo $resultdir >> "/home/ubuntu/check_vars.txt" 
+echo $processdir >> "/home/ubuntu/check_vars.txt" 
+echo $dataname >> "/home/ubuntu/check_vars.txt" 
+echo $datapath >> "/home/ubuntu/check_vars.txt" 
+echo $configname >> "/home/ubuntu/check_vars.txt" 
+echo $configpath >> "/home/ubuntu/check_vars.txt" 
 
 
+echo $bucketname, bucektname
+
+errorlog
+echo errorlog started.
+errorlog_init
+echo errorlog_init started.
+
+## Set up STDOUT and STDERR Monitoring:
+errorlog_background &
+background_pid=$!
+echo $background_pid, "is the pid of the background process"
 
 #environment setup
 export PATH="/home/ubuntu/anaconda3/bin:$PATH"
+echo "$PATH"
 
-source activate lds
+#source activate lds
 
 
 
-userhome="/home/ubuntu" #declaring variables
+userhome="/home/ubuntu/NeuroCAAS/LDS_algo/" #declaring variables
 datastore="LDS/main/localdata/" #declaring variables
 configstore="LDS/main/localconfig/" #declaring variables
 #outstore="epi/scripts/data/lds_2D_linear2D_freq/" #declaring variables
@@ -52,22 +70,22 @@ accessdir "$userhome/$datastore" "$userhome/$configstore" "$userhome/$outstore" 
 
 
 ## Stereotyped download script for data.
-download "$inputpath" "$bucketname" "$datastore" #downloading data to immutable analysis environment
+download "$inputpath" "$bucketname" "$userhome/$datastore" #downloading data to immutable analysis environment
 
 ## Stereotyped download script for config:
-download "$configpath" "$bucketname" "$configstore" # downloading config to immutable analysis environment
+download "$configpath" "$bucketname" "$userhome/$configstore" # downloading config to immutable analysis environment
 ## Check if it's yaml, and if so convert to json:
 ## Reset to correctly get out json:
 configname=$(python $neurocaasrootdir/ncap_utils/yamltojson.py "$userhome"/"$configstore"/"$configname")
 
 ###############################################################################################
 ## Custom bulk processing.
-cd NeuroCAAS/LDS_algo/LDS/main # going to script directory
+cd /home/ubuntu/NeuroCAAS/LDS_algo/LDS/main # going to script directory
 
 
 python clean_KF.py "$userhome/$configstore/$configname" "$userhome/$configstore" "$userhome/$datastore/$dataname" "$userhome/$outstore"
 
-export resultsstore="data/results" # export result directory.
+export resultsstore="data" # export result directory.
 
 
 ## copy the output to our results directory:
